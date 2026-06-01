a81b7626c87cb757b9937526f1039e04df36da17
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-01 12:30:39 +0530

    created a error response struct whose instance is sent to frontend

--- src/error.rs
+++ src/error.rs
@@ -6,3 +6,9 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 use std::fmt;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ErrorResponse {
+    pub status: String,
+    pub message: String,
+}
