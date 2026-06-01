59632663ef17b2564a4e2810368a68aaa43fc81b
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-01 18:18:04 +0530

    added implementation of async function get_user which in SQL SELECT one user

--- src/db.rs
+++ src/db.rs
@@ -77,5 +77,46 @@ pub trait UserExt {
 
 #[async_trait]
 impl UserExt for DBClient {
-      
+      async fn get_user(
+        &self,
+        user_id: Option<Uuid>,
+        name: Option<&str>,
+        email: Option<&str>,
+        token: Option<&str>,
+    ) -> Result<Option<User>, sqlx::Error> {
+        let mut user: Option<User> = None;
+
+        if let Some(user_id) = user_id {
+            user = sqlx::query_as!(
+                User,
+                r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE id = $1"#,
+                user_id
+            ).fetch_optional(&self.pool).await?;
+        } else if let Some(name) = name {
+            user = sqlx::query_as!(
+                User,
+                r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE name = $1"#,
+                name
+            ).fetch_optional(&self.pool).await?;
+        } else if let Some(email) = email {
+            user = sqlx::query_as!(
+                User,
+                r#"SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" FROM users WHERE email = $1"#,
+                email
+            ).fetch_optional(&self.pool).await?;
+             } else if let Some(token) = token {
+            user = sqlx::query_as!(
+                User,
+                r#"
+                SELECT id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole" 
+                FROM users 
+                WHERE verification_token = $1"#,
+                token
+            )
+            .fetch_optional(&self.pool)
+            .await?;
+        }
+
+        Ok(user)
+    }
 }
