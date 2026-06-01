ef061be82bf81f888fabd1d24a98ffb865c8683e
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-01 12:31:35 +0530

    display method for ErrorResponse

--- src/error.rs
+++ src/error.rs
@@ -12,3 +12,9 @@ pub struct ErrorResponse {
     pub status: String,
     pub message: String,
 }
+
+impl fmt::Display for ErrorResponse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", serde_json::to_string(&self).unwrap())
+    }
+}
