1808c8c3a98c6a28d3ccf90aaedfbba12048b195
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-01 15:48:16 +0530

    impl IntoResponse for HttpError {} → Makes Axum automatically convert HttpError into an HTTP response.

--- src/error.rs
+++ src/error.rs
@@ -117,3 +117,9 @@ impl fmt::Display for HttpError {
     }
 }
 impl std::error::Error for HttpError {}
+
+impl IntoResponse for HttpError {
+    fn into_response(self) -> Response {
+        self.into_http_response()
+    }
+}
