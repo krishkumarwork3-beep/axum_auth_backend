e15ad2524d715af47630913a5a537144e9f98b81
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-01 10:30:33 +0530

    created public structure LoginUserDto in dtos.rs

--- src/dtos.rs
+++ src/dtos.rs
@@ -28,3 +28,14 @@ pub struct RegisterUserDto {
     #[serde(rename = "passwordConfirm")]
     pub password_confirm: String,
 }
+
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize)]
+pub struct LoginUserDto {
+    #[validate(length(min = 1, message = "Email is required"), email(message = "Email is invalid"))]
+    pub email: String,
+    #[validate(
+        length(min = 1, message = "Password is required"),
+        length(min = 6, message = "Password must be at least 6 characters")
+    )]
+    pub password: String,
+}
