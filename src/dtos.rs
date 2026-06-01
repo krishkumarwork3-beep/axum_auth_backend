5c56772e28a9872e45fc54088a2dc9930df18d71
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-01 10:48:17 +0530

    created public structure VerifyEmailQueryDto in dtos.rs

--- src/dtos.rs
+++ src/dtos.rs
@@ -127,3 +127,31 @@ fn validate_user_role(role: &UserRole) -> Result<(), validator::ValidationError>
         _ => Err(validator::ValidationError::new("invalid_role")),
     }
 }
+
+#[derive(Debug, Validate, Default, Clone, Serialize, Deserialize)]
+pub struct UserPasswordUpdateDto {
+    #[validate(
+        length(min = 1, message = "New password is required."),
+        length(min = 6, message = "new password must be at least 6 characters")
+    )]
+    pub new_password: String,
+
+    #[validate(
+        length(min = 1, message = "New password confirm is required."),
+        length(min = 6, message = "new password confirm must be at least 6 characters"),
+        must_match(other = "new_password", message="new passwords do not match")
+    )]
+    pub new_password_confirm: String,
+
+    #[validate(
+        length(min = 1, message = "Old password is required."),
+        length(min = 6, message = "Old password must be at least 6 characters")
+    )]
+    pub old_password: String,
+}
+
+#[derive(Serialize, Deserialize, Validate)]
+pub struct VerifyEmailQueryDto {
+    #[validate(length(min = 1, message = "Token is required."),)]
+    pub token: String,
+}
