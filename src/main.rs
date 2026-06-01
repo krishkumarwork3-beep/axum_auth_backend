15712365e25d79cb9720265a00450d81c05a79dd
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-02 05:21:51 +0530

    Purpose: Imports all libraries, modules, and types required by the application in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
+#![allow(unused)]//use it in first line for safe implementation 
 mod models;
 mod config;
 mod dtos;
 mod error;
 mod db;
+
+use std::sync::Arc;
+
+use axum::http::{header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE}, HeaderValue, Method};
+use config::Config;
+use db::DBClient;
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use tower_http::cors::CorsLayer;
+use tracing_subscriber::filter::LevelFilter;
+
 fn main() {
     println!("Hello, world!");
 }
