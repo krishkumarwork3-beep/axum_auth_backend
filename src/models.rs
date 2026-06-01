524d5574ed8e177ffb80547f847edb3694d799fe
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-01 08:45:12 +0530

    added user struct

--- src/models.rs
+++ src/models.rs
@@ -16,3 +16,20 @@ impl UserRole {
         }
     }
 }
+
+#[derive(Debug, Deserialize, Serialize, sqlx::FromRow, sqlx::Type, Clone)]
+pub struct User {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub email: String,
+    pub password: String,
+    pub role: UserRole,
+    pub verified: bool,
+    pub verification_token: Option<String>,
+    pub token_expires_at: Option<DateTime<Utc>>,
+    #[serde(rename = "createdAt")]
+    pub created_at: Option<DateTime<Utc>>,
+    #[serde(rename = "updatedAt")]
+    pub updated_at: Option<DateTime<Utc>>,
+
+}
