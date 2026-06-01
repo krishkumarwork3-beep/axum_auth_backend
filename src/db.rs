f3d2fcbdcd0300d2e2f993e128f1f72a4a186187
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-01 18:22:20 +0530

    added implementation of async function save_user which in SQL INSERT user

--- src/db.rs
+++ src/db.rs
@@ -119,6 +119,7 @@ impl UserExt for DBClient {
 
         Ok(user)
     }
+
     async fn get_users(
         &self,
         page: u32,
@@ -137,5 +138,29 @@ impl UserExt for DBClient {
 
         Ok(users)
     }
-
+    
+    async fn save_user<T: Into<String> + Send>(
+        &self,
+        name: T,
+        email: T,
+        password: T,
+        verification_token: T,
+        token_expires_at: DateTime<Utc>,
+    ) -> Result<User, sqlx::Error> {
+        let user = sqlx::query_as!(
+            User,
+            r#"
+            INSERT INTO users (name, email, password,verification_token, token_expires_at) 
+            VALUES ($1, $2, $3, $4, $5) 
+            RETURNING id, name, email, password, verified, created_at, updated_at, verification_token, token_expires_at, role as "role: UserRole"
+            "#,
+            name.into(),
+            email.into(),
+            password.into(),
+            verification_token.into(),
+            token_expires_at
+        ).fetch_one(&self.pool)
+        .await?;
+        Ok(user)
+    }
 }
