b0292d1294df66a7b05815094ca5b58bc1f5c944
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-01 12:42:25 +0530

    created a ErrorMessage enum

--- src/error.rs
+++ src/error.rs
@@ -18,3 +18,19 @@ impl fmt::Display for ErrorResponse {
         write!(f, "{}", serde_json::to_string(&self).unwrap())
     }
 }
+
+#[derive(Debug, PartialEq)]
+pub enum ErrorMessage {
+    EmptyPassword,
+    ExceededMaxPasswordLength(usize),
+    InvalidHashFormat,
+    HashingError,
+    InvalidToken,
+    ServerError,
+    WrongCredentials,
+    EmailExist,
+    UserNoLongerExist,
+    TokenNotProvided,
+    PermissionDenied,
+    UserNotAuthenticated,
+}
