81ccbf6e3444dfa401a5eac637a0d453fadb1118
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-02 05:50:23 +0530

    created a pool variable where we create and initialize a PostgreSQL connection pool ok(pool) confirms successful database connection and stores the pool and Err(err) logs the database error and terminates the application.

--- src/main.rs
+++ src/main.rs
@@ -30,4 +30,19 @@ async fn main() {
     dotenv().ok();
 
     let config = Config::init();
+    
+    let pool = match PgPoolOptions::new()
+            .max_connections(10)
+            .connect(&config.database_url)
+            .await
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
 }
