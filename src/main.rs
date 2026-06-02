a042bedc3c71840d3b7a78770b560a5c414f2ab2
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-02 05:53:10 +0530

    CORS configuration specifies which frontend websites, HTTP methods, and request headers are allowed to access your backend API from a browser.

--- src/main.rs
+++ src/main.rs
@@ -45,4 +45,10 @@ async fn main() {
             std::process::exit(1);
         }
     };
+
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE])
+        .allow_credentials(true)
+        .allow_methods([Method::GET, Method::POST,Method::PUT]);
 }
