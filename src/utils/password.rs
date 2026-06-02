2d01714e727d7b5cedcfd578be11e8f071daba43
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-02 13:07:49 +0530

    Convert the input parameter into a String regardless of its original type.

--- src/utils/password.rs
+++ src/utils/password.rs
@@ -13,5 +13,5 @@ use crate::error::ErrorMessage;
 
 const MAX_PASSWORD_LENGTH: usize = 64;
 pub fn hash(password: impl Into<String>) -> Result<String, ErrorMessage> {
-    
+    let password = password.into();
 }
