580f36b0b0409c6645b9223285ab55dd9e5bb46c
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-02 12:45:54 +0530

    import Argon2 password hashing utilities and custom error types

---
+++ src/utils/password.rs
@@ -0,0 +1,12 @@
+use argon2::{
+    password_hash::{
+        rand_core::OsRng,
+        PasswordHash,
+        PasswordHasher,
+        PasswordVerifier,
+        SaltString
+    },
+    Argon2,
+};
+
+use crate::error::ErrorMessage;
