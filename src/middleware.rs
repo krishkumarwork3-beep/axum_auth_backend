f0d79d7c9426c54662d9035574ec0ee627781ac3
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-03 19:30:34 +0530

    Imports middleware, authentication, request, response, and application state utilities.

---
+++ src/middleware.rs
@@ -0,0 +1,20 @@
+use std::sync::Arc;
+
+use axum::{
+    extract::Request,
+    http::{header, StatusCode},
+    middleware::Next,
+    response::IntoResponse,
+    Extension
+};
+
+use axum_extra::extract::cookie::CookieJar;
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    db::UserExt,
+    error::{ErrorMessage, HttpError},
+    models::{User, UserRole},
+    utils::token,
+    AppState
+};
