627bad142063a3cec1be53c89f72a73238930f00
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-03 20:43:01 +0530

    Continue Authorized Request Allows the request to proceed if role verification succeeds.

--- src/middleware.rs
+++ src/middleware.rs
@@ -95,4 +95,6 @@ pub async fn role_check(
      if !required_roles.contains(&user.user.role) {
         return Err(HttpError::new(ErrorMessage::PermissionDenied.to_string(), StatusCode::FORBIDDEN));
     }
+
+    Ok(next.run(req).await)
 }
