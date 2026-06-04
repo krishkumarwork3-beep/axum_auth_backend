dacc155f44bcc0d132026043a0a074e1b1b274ea
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-04 12:00:29 +0530

    send_verification_email Function Sends an email verification message to newly registered users.

--- src/mail/mails.rs
+++ src/mail/mails.rs
@@ -1,1 +1,8 @@
 use super::sendmail::send_email;
+pub async fn send_verification_email(
+    to_email: &str,
+    username: &str,
+    token: &str
+) -> Result<(), Box<dyn std::error::Error>> {
+    
+}
