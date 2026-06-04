ee1893833e56448ba785910434b45e2a83315133
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-04 14:17:40 +0530

    Imports routing, validation, cookies, database, JWT, email, and application utilities.

---
+++ src/handler/auth.rs
@@ -0,0 +1,8 @@
+use std::sync::Arc;
+
+use axum::{extract::Query, http::{header, HeaderMap, StatusCode}, response::{IntoResponse, Redirect}, routing::{get, post}, Extension, Json, Router};
+use axum_extra::extract::cookie::Cookie;
+use chrono::{Utc, Duration};
+use validator::Validate;
+
+use crate::{db::UserExt, dtos::{ForgotPasswordRequestDto, LoginUserDto, RegisterUserDto, ResetPasswordRequestDto, Response, UserLoginResponseDto, VerifyEmailQueryDto}, error::{ErrorMessage, HttpError}, mail::mails::{send_forgot_password_email, send_verification_email, send_welcome_email}, utils::{password, token}, AppState};
