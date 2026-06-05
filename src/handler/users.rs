30e68da237579f6bfa3f8de3f384ffe6ceec4f09
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-05 16:02:44 +0530

    Create Router(Initializes a new Axum router for user endpoints.)

--- src/handler/users.rs
+++ src/handler/users.rs
@@ -6,5 +6,5 @@ use validator::Validate;
 use crate::{db::UserExt, dtos::{FilterUserDto, NameUpdateDto, RequestQueryDto, Response, RoleUpdateDto, UserData, UserListResponseDto, UserPasswordUpdateDto, UserResponseDto}, error::{ErrorMessage, HttpError}, middleware::{role_check, JWTAuthMiddeware}, models::UserRole, utils::password, AppState};
 
 pub fn users_handler() -> Router {
-    
+    Router::new()
 }
