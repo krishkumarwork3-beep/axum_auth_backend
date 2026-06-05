6190aa828d866a5348130fd5d3c84175a82e0b6e
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-05 17:25:48 +0530

    Extract Authenticated User(Retrieves authenticated user data from middleware.)

--- src/handler/users.rs
+++ src/handler/users.rs
@@ -102,6 +102,7 @@ pub async fn update_user_name(
 
 pub async fn update_user_role(
     Extension(app_state): Extension<Arc<AppState>>,
+    Extension(user): Extension<JWTAuthMiddeware>,
 ) -> Result<impl IntoResponse, HttpError> {
     
 }
