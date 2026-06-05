17b2657f144381a1b34695c14716801b1d6975b0
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-05 17:09:41 +0530

    Update Name In Database

--- src/handler/users.rs
+++ src/handler/users.rs
@@ -83,4 +83,7 @@ pub async fn update_user_name(
 
     let user_id = uuid::Uuid::parse_str(&user.id.to_string()).unwrap();
 
+    let result = app_state.db_client.
+        update_user_name(user_id.clone(), &body.name)
+
 }
