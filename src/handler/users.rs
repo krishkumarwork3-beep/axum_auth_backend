d45357bc30b3d4b87fb0cc09ece5f6fb2ff1d02e
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-05 15:59:42 +0530

    Imports routing, middleware, validation, DTOs, authentication, database, and utility modules.

---
+++ src/handler/users.rs
@@ -0,0 +1,7 @@
+use std::sync::Arc;
+
+use axum::{extract::Query, middleware, response::IntoResponse, routing::{get, put}, Extension, Json, Router};
+use validator::Validate;
+
+use crate::{db::UserExt, dtos::{FilterUserDto, NameUpdateDto, RequestQueryDto, Response, RoleUpdateDto, UserData, UserListResponseDto, UserPasswordUpdateDto, UserResponseDto}, error::{ErrorMessage, HttpError}, middleware::{role_check, JWTAuthMiddeware}, models::UserRole, utils::password, AppState};
+
