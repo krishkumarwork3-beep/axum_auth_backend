9e1a89cb2be6a1b14027d3749f52ba9c67b36aad
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-06 13:41:18 +0530

    Imports routing, middleware, tracing, authentication, and shared application state utilities.

---
+++ src/routes.rs
@@ -0,0 +1,6 @@
+use std::sync::Arc;
+
+use axum::{middleware, Extension, Router};
+use tower_http::trace::TraceLayer;
+
+use crate::{handler::{auth::auth_handler, users::users_handler}, middleware::auth, AppState};
